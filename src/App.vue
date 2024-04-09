d9ceb68f4fb2ac658d852b4ea39ed33025c77343
FiLLi <34349763+Filli-z@users.noreply.github.com>
2024-04-09 16:32:18 +0300

    final

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<script setup>
+import NavBarView from "./components/NavBarView.vue";
+import HeroPageView from "./components/HeroPageView.vue";
+import BodyView from "./components/BodyView.vue";
+</script>
+
+<template>
+  <q-layout view="hHh lpR lFf">
+    <div class="bg-image">
+      <NavBarView />
+      <HeroPageView />
+    </div>
+    <BodyView />
+  </q-layout>
+</template>
+
+<style scoped>
+.bg-image {
+  background-image: linear-gradient(to bottom, #26c2b9, #288be7);
+  background-repeat: no-repeat;
+}
+</style>
